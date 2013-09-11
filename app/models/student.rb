33e95e8440334c39a346f452f3e9ba8409e6bd84
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-11 16:25:55 -0500

    .

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, uniqueness: true
+  validates :email, :format => {:with => /\w+@\w+.\w{2,}/, message: 'Invalid email'}
+  validates :age, :numericality => { greater_than: 5 }
+  validates :phone, format: { with: /.{10}/, message: 'Beep!' }
+
+  def name
+    first_name + " " + last_name
+  end
+
+  def age
+    Date.today.year - birthday.year
+  end
 end
